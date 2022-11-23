9c7af520988d11af966ee982f4368297eaf3ab76
danifaso02 <danifaso02@gmail.com>
2022-11-23 16:54:11 +0100

    aggiunta due immagini + stile

---
+++ src/components/AppHero.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  data() {
+    return {
+      imgOne: "src/assets/vue.svg",
+      imgTwo: "vite.svg"
+    }
+  }
+}
+</script>
+
+<template>
+    <div>
+        <img :src="`${imgOne}`" alt="Logo Vue">
+        <img :src="`${imgTwo}`" alt="Logo Vite">
+    </div>
+</template>
+
+<style scoped>
+div{
+    display: flex;
+    justify-content: space-around;
+}
+img{
+    width: 9.375rem;
+    margin: .3125rem .625rem;
+}
+</style>
